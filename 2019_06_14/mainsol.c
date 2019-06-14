900988194169a2f70858f19febb27a905d8747a1
Mario Londono <londono.mario@correounivalle.edu.co>
2019-06-14 12:57:41 -0500

    adicionando README.md

--- 2019_06_14/mainsol.c
+++ 2019_06_14/mainsol.c
@@ -1,13 +1,36 @@
 #include "solucion.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+
 
 int main(int argc, char** argv) {
   int x = 20;
+  pid_t pid;
+  pid = fork();
 
-  printf("fibonacci(%d) = ",x);
-  x = fibonacci(x);
-  printf("%d\n",x);
-  guardarEntero("archivo",x);
+  if (pid<0){
+	printf("fork failed");
+	}
+
+ else if(pid==0){
+    printf("Soy el hijo\n");
+    printf("fibonacci(%d) = ",x);
+    x = fibonacci(x);
+    printf("%d\n",x);
+    guardarEntero("archivo",x);
+  } 
+	else {
+    	int status;
+	    wait(&status);
+	    int ans = leerEntero("archivo")* 100;
+	    printf("soy el padre \n espero a que termine mi hijo \n leo el valor que calculo mi hijo del archivo 'x' \n");
+	    printf("multiplico el resultado por 100 e imprimo por pantalla %d",ans);
+	  }
 
   return 0;
 }
